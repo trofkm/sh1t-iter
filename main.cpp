1b4d4a002b96572a2c4296c19d615be6d234ea9f
k.trofimov <trofkm@gmail.com>
2023-03-04 17:19:07 +0300

    Change cmake

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <ranges>
 #include <vector>
 
+// can miss some elements in case of empty file somewhere in the vector
 void shit_iterate(std::vector<std::fstream> &files) {
     for (auto &file: files) {
         std::string line;
@@ -13,18 +14,31 @@ void shit_iterate(std::vector<std::fstream> &files) {
             // do some shizzy stuff
         }
         file.close();
-        std::remove_if(files.begin(), files.end(), [](const std::fstream &f){return f.eof();});
-
+        std::ranges::remove_if(files, [](const std::fstream &f) { return f.eof(); });
     }
+}
+
+void good_iterate(std::vector<std::fstream> &files) {
+    for (auto &file: files) {
+        std::string line;
+        std::cout << "iterating over file " << std::endl;
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+            // do some shizzy stuff
+        }
+        file.close();
+    }
+    std::ranges::remove_if(files, [](const std::fstream &f) { return f.eof(); });
 }
 
 
 int main() {
     std::vector<std::fstream> files;
-    files.emplace_back("F:\\Code\\FIles\\files\\1.txt");
-    files.emplace_back("F:\\Code\\FIles\\files\\2.txt");
-    files.emplace_back("F:\\Code\\FIles\\files\\3.txt");
-//    files.emplace_back("F:\\Code\\FIles\\files\\empty.txt");
-    shit_iterate(files);
+    // try to move around empty file emplace_back
+    files.emplace_back(R"(F:\Code\FIles\files\empty.txt)");
+    files.emplace_back(R"(F:\Code\FIles\files\1.txt)");
+    files.emplace_back(R"(F:\Code\FIles\files\2.txt)");
+    files.emplace_back(R"(F:\Code\FIles\files\3.txt)");
+    good_iterate(files);
     return 0;
 }
